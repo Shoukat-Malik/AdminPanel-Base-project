27be43b2dd68b8ba266ca327be9154a1f5d1aea9
Shoukat Malik <shoukat.malik@danatonline.com>
2022-04-21 01:30:34 +0500

    add environment tag in _Layout.cshtml and _Layout.Login

---
+++ CrudOperationsApp/ViewComponents/TestViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudOperationsApp.ViewComponents
+{
+    public class TestViewComponent:ViewComponent 
+    {
+        public TestViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
